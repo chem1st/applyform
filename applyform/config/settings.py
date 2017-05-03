e990f8261ceb8814223a53779a4ab53991976047
chem1st <chem1st@devMachine.(none)>
2017-05-03 11:53:28 +0300

    allowes hosts updated

--- applyform/config/settings.py
+++ applyform/config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = get_env_variable("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'osmuss.muctr.ru']
+ALLOWED_HOSTS = ['127.0.0.1', '193.232.56.50', 'osmuss.muctr.ru']
 
 DEFAULT_FROM_EMAIL = 'osmuss@muctr.ru'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
